112e55ba2bce0c367221c62b43f7b760a8cce37c
RobbyOdannix <89948248+RobbyOdannix@users.noreply.github.com>
2024-12-08 19:17:28 -0400

    Initial push

---
+++ src/app/adminpanel/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import testItems from "../data/testItems";
+export default function AdminPanel() {
+
+
+
+
+  return (
+    <div>admin panel</div>
+    
+    
+  )
+}
---
+++ src/app/data/testItems.js
@@ -0,0 +1,26 @@
+export default testItems=[
+    {
+        name: "Tornillos XL",
+        category: "tornillos",
+        subcategory: "roscas",
+        quantity: 15,
+    },
+    {
+        name: "Tornillos SM",
+        category: "tornillos",
+        subcategory: "roscar",
+        quantity: 15,
+    },
+    {
+        name: "Martillo Goma",
+        category: "herramienta",
+        subcategory: "",
+        quantity: 10,
+    },
+    {
+        name: "Pintura Azul",
+        category: "Decoracion",
+        subcategory: "Pintura",
+        quantity: 150,
+    }
+]
